5b256882806149ccf41f393cdf657bd999bfc06e
suckowbiz <tobias@suckow.biz>
2019-08-29 14:02:53 +0200

    Wait for test connection instead of sleep.

--- main_test.go
+++ main_test.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -13,7 +13,6 @@ import (
 )
 
 func TestRESTEndpointsAvailableIntegration(t *testing.T) {
-	assert := assert.New(t)
 	port := 61978
 
 	tests := []struct {
@@ -22,7 +21,7 @@ func TestRESTEndpointsAvailableIntegration(t *testing.T) {
 	}{
 		{
 			endpoint: "http://localhost:" + strconv.Itoa(port) + "/update",
-			method:   http.MethodPost,
+			method:   http.MethodGet,
 		},
 	}
 
@@ -33,18 +32,32 @@ func TestRESTEndpointsAvailableIntegration(t *testing.T) {
 		log.Fatal(server.ListenAndServe())
 	}()
 
-	// Avoid: Post http://localhost:61978/update: dial tcp 127.0.0.1:61978: connect: connection refused
-	time.Sleep(time.Second * 5)
+	// Avoid: http://localhost:61978/update: dial tcp 127.0.0.1:61978: connect: connection refused
+	waitForConnection(t, "http://localhost:"+strconv.Itoa(port))
 
 	for _, test := range tests {
 		req, _ := http.NewRequest(test.method, test.endpoint, nil)
 		res, err := http.DefaultClient.Do(req)
-		if res != nil {
-			//nolint
-			defer res.Body.Close()
-		}
 		require.NoError(t, err)
 		require.NotNil(t, res)
-		assert.NotEqual(http.StatusNotFound, res.StatusCode)
+		assert.NotEqual(t, http.StatusNotFound, res.StatusCode)
+		_ = res.Body.Close()
+	}
+}
+
+func waitForConnection(t *testing.T, url string) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+	for {
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Printf("%s", err.Error())
+			if strings.Contains(err.Error(), "connection refused") {
+				continue
+			}
+			require.NoError(t, err)
+		}
+		_ = resp.Body.Close()
+		break
 	}
 }
